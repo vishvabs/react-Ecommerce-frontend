d22e2a934a505e104169fb09797c56a93f49e55c
vishvabs <vishvabs@gmail.com>
2022-07-14 13:14:21 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const sliderItems = [
+    {
+        id : 1,
+        img: "https://i.pinimg.com/originals/7b/11/e8/7b11e86120d2eb3207012b9b67c44093.jpg",
+        title: "SUMMER SALE",
+        desc: "GEt this quiick sample text to the desic",
+        bg: "f5fafd"
+    },
+    {
+        id : 2,
+        img: "https://i.pinimg.com/originals/7b/11/e8/7b11e86120d2eb3207012b9b67c44093.jpg",
+        title: "GET NOW FOR 50% OFF",
+        desc: "GEt this quiick sample text to the desic new same for season",
+        bg: "fcf1ed"
+    },
+]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Annnouncement from '../components/Annnouncement'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
+const Home = () => {
+  return (
+    <div>
+        <Annnouncement/>
+        <Navbar/>
+        <Slider/>
+    </div>
+  )
+}
+
+export default Home
